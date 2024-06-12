40db95a9429d08965d1309191de4821764c28630
judeb0x <ginny34800@gmail.com>
2024-06-12 13:39:42 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
-
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("submit-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    document.getElementById("message").innerHTML = "Thank you for your opinion! We gladly appreciate your feedback and we can't wait for the next chapter to drop :)";
-  });
-});
